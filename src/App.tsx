a856165b05e2a08042a0528973fa3c77a0286bc9
alestojanovic <aleksandar.stojanovic@productdock.com>
2022-11-19 14:42:40 +0100

    feat: solved tracking user click position

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
 import "./App.css";
 
+type TPoint = {
+  x: number;
+  y: number;
+};
+
 function App() {
-  return <div></div>;
+  const [points, setPoints] = useState<Array<TPoint>>([]);
+  const handlePlaceCircle = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { clientX, clientY } = e;
+    setPoints([...points, { x: clientX, y: clientY }]);
+  };
+
+  return (
+    <div className="app" onClick={handlePlaceCircle}>
+      {points.map((point, index) => (
+        <div
+          key={index}
+          className="point"
+          style={{ left: point.x - 6 + "px", top: point.y - 10 + "px" }}
+        ></div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
