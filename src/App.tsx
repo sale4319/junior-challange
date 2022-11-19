ac28c664607f0010a9c23700c2a3b2201a2d74b3
alestojanovic <aleksandar.stojanovic@productdock.com>
2022-11-19 14:48:19 +0100

    feat: added undo button

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,31 @@ type TPoint = {
 
 function App() {
   const [points, setPoints] = useState<Array<TPoint>>([]);
+
   const handlePlaceCircle = (e: React.MouseEvent<HTMLDivElement>) => {
     const { clientX, clientY } = e;
     setPoints([...points, { x: clientX, y: clientY }]);
   };
 
+  const handleUndo = () => {
+    const newPoints = [...points];
+    newPoints.pop();
+    setPoints(newPoints);
+  };
+
   return (
-    <div className="app" onClick={handlePlaceCircle}>
-      {points.map((point, index) => (
-        <div
-          key={index}
-          className="point"
-          style={{ left: point.x - 6 + "px", top: point.y - 10 + "px" }}
-        ></div>
-      ))}
-    </div>
+    <>
+      <button onClick={handleUndo}>Undo</button>
+      <div className="app" onClick={handlePlaceCircle}>
+        {points.map((point, index) => (
+          <div
+            key={index}
+            className="point"
+            style={{ left: point.x - 6 + "px", top: point.y - 10 + "px" }}
+          ></div>
+        ))}
+      </div>
+    </>
   );
 }
 
